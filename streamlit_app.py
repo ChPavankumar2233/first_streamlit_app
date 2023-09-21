3395e366055e314b491d81ff46b22c6e636af656
ChPavankumar2233 <92834618+ChPavankumar2233@users.noreply.github.com>
2023-09-21 18:56:50 +0530

    Update streamlit_app.py
    taking entry from user

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 import streamlit
 import pandas
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/Kiwi")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/ +fruit_choice")
 
 
 
