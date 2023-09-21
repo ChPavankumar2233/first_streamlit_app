ed51f54f0c159a0095a21f08d40ef1765422597a
ChPavankumar2233 <92834618+ChPavankumar2233@users.noreply.github.com>
2023-09-21 23:06:43 +0530

    Update streamlit_app.py
    added

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,4 +46,4 @@ series=['jackfruit','papaya','kiwi','guava']
 my_data_rows = my_cur.fetchall()
 streamlit.header("The Fruit load list contains:")
 streamlit.dataframe(my_data_rows)
-streamlit.dataframe.insert(0,series)
+streamlit.dataframe.insert(0,0,series)
