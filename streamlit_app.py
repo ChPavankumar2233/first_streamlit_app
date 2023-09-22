4c1a08377fb9270cd4d8f4fdbbdef7f7892bddba
ChPavankumar2233 <92834618+ChPavankumar2233@users.noreply.github.com>
2023-09-22 10:16:45 +0530

    Update streamlit_app.py
    code is seperated accordingly

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import requests
 import snowflake.connector
 
 
@@ -21,29 +22,25 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
-
-
 streamlit.dataframe(fruits_to_show)
+
+
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 # write your own comment -what does the next line do? ))
 # write your own comment - what does this do?
-
-
-
-import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
+
+
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list")
-#my_cur.execute("insert into fruit_load_list values('jackfruit','papaya','kiwi','guava')")
-series=['jackfruit','papaya','kiwi','guava']
-my_data_rows = my_cur.fetchall()
+my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+my_data_row = my_cur.fetchone()
 streamlit.header("The Fruit load list contains:")
-streamlit.dataframe(my_data_rows)
-streamlit.dataframe.concat(series)
+streamlit.dataframe(my_data_row)
