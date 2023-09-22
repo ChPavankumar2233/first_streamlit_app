48b57e20d4d04adb9d10bdc02408e46acecf9061
ChPavankumar2233 <92834618+ChPavankumar2233@users.noreply.github.com>
2023-09-22 12:41:13 +0530

    Update streamlit_app.py
    made changes in add new fruit list added connections

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,12 +51,21 @@ def get_fruit_load_list():
          return my_cur.fetchall()
 
 #Add a button to load fruit load list
-if streamlit.button('get fruit load list'):
+if streamlit.button('get fruit list'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows =get_fruit_load_list()
+    my_cnx.close()
     streamlit.dataframe(my_data_rows)
 
+
+def insert_row_snowflake(new_fruit):
+    with my_cnx.cursor() as my_cur:
+         return "Thanks for adding new fruit" +new_fruit
+         my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values('from streamlit')")
+        
+add_my_fruit = streamlit.text_input('What fruit would you add?')
+if streamlit.button('Add a fruit from the list'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    back_from_function = insert_row_snowflake(add_my_fruit)
+    streamlit.text(back_from_function)
 streamlit.stop()
-add_my_fruit = streamlit.text_input('What fruit would you add?', 'Jackfruit')
-streamlit.write('Thanks for adding ', add_my_fruit)
-my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values('from streamlit')")
